d19805cd741a7ac176ddd7606d8ba55f02e78363
TheFulum <lipouski.daniil@gmail.com>
2025-03-03 19:49:40 +0300

    update

--- webshop/obj/Debug/net8.0/webshop.AssemblyInfo.cs
+++ webshop/obj/Debug/net8.0/webshop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webshop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f80c362dfec9a66c022645e7ea2494d66fc197ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bde34955b05fbdfd8dfaa70216f2c597543c5d95")]
 [assembly: System.Reflection.AssemblyProductAttribute("webshop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webshop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
